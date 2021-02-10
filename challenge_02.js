c93e485a511f55fdf5c71cb232ae4449833d83a6
infi1 <78482456+infi1@users.noreply.github.com>
2021-02-10 09:05:28 +0100

    solution

--- challenge_02.js
+++ challenge_02.js
@@ -14,4 +14,23 @@ Alter 13-17 // Cola
 
 */
 
+let firstName = prompt("Bitte Namen eingeben:");
+let age = prompt("Bitte Alter eingeben:");
 
+if (firstName == "") { // empty string
+    firstName = "Unbekannt";
+}
+
+console.log(firstName + ", " + age + " Jahre alt")
+
+if (age >= 0 && age <= 5 && age != "") { // 0-5
+    console.log("Milch.");
+} else if (age >= 6 && age <= 12) { // 6-12
+    console.log("Milch und Saft.");
+} else if (age >= 13 && age <= 17) { // 13-17
+    console.log("Milch, Saft und Cola.");
+} else if (age >= 18) { // 18+
+    console.log("Milch, Saft, Cola und Wein.");
+} else { // negative number or NaN
+    console.log("Ungültiges Alter.")
+};
